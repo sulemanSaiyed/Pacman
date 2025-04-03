809ed4b7aa14db86107fd6e63cc500ac3ac99735
pandu-12-2000 <crackivar@gmail.com>
2025-04-03 14:32:13 +0530

    creating game panel it act as content app calss is a container

---
+++ src/PacMan.java
@@ -0,0 +1,14 @@
+import  javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashSet;
+import java.util.Random;
+import javax..swing.*;
+
+public class PacMan extends  JPanel{
+  private int rowCount=21;
+  private int columCount=19;
+  private int tileSize=32;
+  private  int boardWidth=columCount*tileSize;
+  private  int boardHeight=rowCount*tileSize;
+}
