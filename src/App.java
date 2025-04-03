91fcf4be7aeb809cf250acdf63d76fd5eaec2096
pandu-12-2000 <crackivar@gmail.com>
2025-04-03 13:49:55 +0530

    determining size of a window

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     int rowCount=21;
+     int columCount=19;
+     int tileSize=32;
+     int boardWidth=columCount*tileSize;
+     int boardHeight=rowCount*tileSize;
+   
+
+    }
+
+}
+       
