9aff258b233dda3e01f04ebba92e9ac0b95df973
pandu-12-2000 <crackivar@gmail.com>
2025-04-06 23:25:34 +0530

    added tile map for the game

--- src/PacMan.java
+++ src/PacMan.java
@@ -1,4 +1,3 @@
-import  javax.swing.JPanel;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.HashSet;
@@ -51,6 +50,35 @@ public class PacMan extends  JPanel{
   
   private Image packManRightImage;
 
+  
+Pinned by @KennyYipCoding
+@KennyYipCoding
+5 months ago
+//X = wall, O = skip, P = pac man, ' ' = food
+    //Ghosts: b = blue, o = orange, p = pink, r = red
+    private String[] tileMap = {
+        "XXXXXXXXXXXXXXXXXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X                 X",
+        "X XX X XXXXX X XX X",
+        "X    X       X    X",
+        "XXXX XXXX XXXX XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXrXX X XXXX",
+        "O       bpo       O",
+        "XXXX X XXXXX X XXXX",
+        "OOOX X       X XOOO",
+        "XXXX X XXXXX X XXXX",
+        "X        X        X",
+        "X XX XXX X XXX XX X",
+        "X  X     P     X  X",
+        "XX X X XXXXX X X XX",
+        "X    X   X   X    X",
+        "X XXXXXX X XXXXXX X",
+        "X                 X",
+        "XXXXXXXXXXXXXXXXXXX" 
+    };
 HashSet<Block>walls;
 HashSet<Block>foods;
 HashSet<Block>ghosts;
